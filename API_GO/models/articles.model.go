998cfaf0d546f700ed52c4dc9439c36c827244f2
Kévin Alves <78157227+KevOneRedOne@users.noreply.github.com>
2023-11-02 21:13:11 +0100

    Delete API_GO directory

--- API_GO/models/articles.model.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Article struct {
-	ID      uint   `json:"id" gorm:"primary_key"`
-	Title   string `json:"title"`
-	Description string `json:"description"`
-	Date string `json:"date"`
-}
